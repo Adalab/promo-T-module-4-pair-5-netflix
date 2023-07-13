5e5e19a41682998bb699dd1939fbdc3f6c3374a8
kathimansilla <mansillakatherina@gmail.com>
2023-07-13 11:34:02 +0200

    create endpoint and connect to DB

--- src/index.js
+++ src/index.js
@@ -11,3 +11,30 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+async function connectDB () {
+  const connection = await mysql
+  .createConnection({
+    host: 'sql.freedb.tech',
+    database: 'freedb_netflix_DB',
+    user: 'freedb_módulo-4-pair-5',
+    password: 'ZS2c?%QehHXHj*9'
+  })
+  await connection.connect();
+  console.log(`conexión con ${connection.threadID}`);
+  return connection;
+};
+
+connectDB();
+
+server.get('/movies', async (req, res) => {
+  const select = 'SELECT * FROM movies';
+  const connect = await connectDB();
+  const [result, cols] = await connect.query(select);
+  console.log(result);
+  connect.end();
+  res.json({
+    success: true,
+    movies:  results
+  });
+});
