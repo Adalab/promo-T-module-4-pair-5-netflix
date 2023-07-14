4228815c1424ec54887d19a377e206e6aa37bf73
“AndreaFerreiro” <andreafga28@gmail.com>
2023-07-14 11:21:33 +0200

    change database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const mysql = require('mysql2/promise');
 // create and config server
 const server = express();
 server.use(cors());
@@ -18,10 +18,10 @@ async function connectDB () {
     host: 'sql.freedb.tech',
     database: 'freedb_netflix_DB',
     user: 'freedb_módulo-4-pair-5',
-    password: 'ZS2c?%QehHXHj*9'
+    password: '7Um5cp%db$dS?Gb'
   })
   await connection.connect();
-  console.log(`conexión con ${connection.threadID}`);
+  console.log(`conexión con ${connection.threadId}`);
   return connection;
 };
 
@@ -35,7 +35,7 @@ server.get('/movies', async (req, res) => {
   connect.end();
   res.json({
     success: true,
-    movies:  results
+    movies:  result
   });
 });
 
